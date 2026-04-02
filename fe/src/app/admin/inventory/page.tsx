d28d742c98a60306bd20790c90290e66706b51bb
Thanhhnse171162 <thanhhnse171162@fpt.edu.vn>
2026-04-02 14:31:16 +0700

    Delete inventory-admin pages (overview and stock)

--- fe/src/app/admin/inventory/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function AdminInventoryAliasPage() {
-  // Alias /admin/inventory -> route group (admin)/inventory-admin
-  redirect('/inventory-admin')
-}
-
--- fe/src/app/admin/inventory/stock/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function AdminItemStockAliasPage() {
-  redirect('/inventory-admin/stock')
-}
-
