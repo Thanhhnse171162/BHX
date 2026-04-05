d865d6c8c81e83aa39c13e3f8d9b307d057b066d
Thanhhnse171162 <thanhhnse171162@fpt.edu.vn>
2026-04-05 19:07:32 +0700

    Fix endpoint to use kebab-case /product-batch/batch/adjust-quantity

--- fe/src/services/product-batch-api.service.ts
+++ fe/src/services/product-batch-api.service.ts
@@ -110,7 +110,7 @@ export class ProductBatchAPIService {
 
   static async adjustBatchQuantity(body: AdjustBatchQuantityDTO): Promise<boolean> {
     try {
-      const response = await localApiClient.post(`/ProductBatch/batch/adjust-quantity`, {
+      const response = await localApiClient.post(`/product-batch/batch/adjust-quantity`, {
         batchId: body.batchId,
         actualQuantity: body.actualQuantity,
         locationType: body.locationType,
